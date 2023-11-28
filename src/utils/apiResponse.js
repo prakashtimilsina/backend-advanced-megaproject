4632d92cf5248329a0e4c7d9ede1c63730ca7eb6
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-11-27 20:17:18 -0600

    Update apiResponse.js

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -3,6 +3,6 @@ class ApiResponse {
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.success = statusCode < 400
+        this.success = statusCode < 400 // code for response.
     }
 }
