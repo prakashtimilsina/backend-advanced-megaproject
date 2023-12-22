e8bab55916ce8f32cfa1f037884891c193116b2d
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-12-21 21:20:39 -0600

    refresh and access token generate

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
-import { User } from "../models/user.models";
-import { ApiError } from "../utils/apiErrorHandler";
-import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/apiErrorHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async(req, res, next)=>{
